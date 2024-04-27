1003fe4cc71ab501b2456ec732c887e21eec31f0
Adrian <adyoblu2000@yahoo.com>
2024-04-27 10:43:08 +0300

    build

--- src/app/page.js
+++ src/app/page.js
@@ -17,13 +17,13 @@ export default function Home() {
           mainHeader={'About us'} />
           <div className="dark:text-white max-w-md mx-auto text-gray-500 mt-4 flex-col gap-4">
             <p className="">
-              In the city's bustling heart lies Mamma Mia Pizzeria, a cozy haven where the legacy of Nonna Lucia's secret dough recipe lives on. Here, every pizza is a canvas of flavors, painted with the freshest local ingredients and a touch of Italian tradition.
+            In the city&apos;s bustling heart lies Mamma Mia Pizzeria, a cozy haven where the legacy of Nonna Lucia&apos;s secret dough recipe lives on. Here, every pizza is a canvas of flavors, painted with the freshest local ingredients and a touch of Italian tradition.
             </p>
             <p>
-              Over a decade of shared memories and savored moments have made Mamma Mia's more than a restaurant—it's a community cornerstone. It's where families gather, love stories blossom, and the simple joy of a well-crafted pizza brings smiles to faces young and old.
+              Over a decade of shared memories and savored moments have made Mamma Mia&apos;s more than a restaurant—it&apos;s a community cornerstone. It&apos;s where families gather, love stories blossom, and the simple joy of a well-crafted pizza brings smiles to faces young and old.
             </p>
             <p>
-              Led by Mario, Nonna Lucia's grandson, Mamma Mia Pizzeria invites you to be part of our story. Join us for a slice, and let's make memories that linger long after the last bite.
+              Led by Mario, Nonna Lucia&apos;s grandson, Mamma Mia Pizzeria invites you to be part of our story. Join us for a slice, and let&apos;s make memories that linger long after the last bite.
             </p>
           </div>
           <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold dark:text-white">
